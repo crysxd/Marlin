bde35c09ff4f45e5acfa960e6fc7c2a920027e70
Christian Würthner <c.wurthner@gmail.com>
2020-04-13 13:59:24 +0200

    E_STEPS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -933,7 +933,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -2.33 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -3.24 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
