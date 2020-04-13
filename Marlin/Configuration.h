03dc6e0d8b0925fb36cd0ef99c004aa1b6f0c963
Christian Würthner <c.wurthner@gmail.com>
2020-04-13 14:13:09 +0200

    Update measured values for new setup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -713,7 +713,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 106.63 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 413.26 }
 
 /**
  * Default Max Feed Rate (mm/s)
